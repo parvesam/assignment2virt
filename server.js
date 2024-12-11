f99680effa274ec9e0644bdf40fcafdb3b6ed140
parvesam <parvesam@sheridancollege.ca>
2024-12-11 12:21:55 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Dockerized loool Node.js App!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+});
