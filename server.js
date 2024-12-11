6950affddda5999c3054d3dc2305f41cb53561b9
parvesam <parvesam@sheridancollege.ca>
2024-12-11 13:05:27 -0500

    testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Samira here');
+    res.send('Hello Samira!');
 });
 
 app.listen(PORT, () => {
