cd091d0e8cf7921c040c69d6a76fa22a51975ec2
parvesam <parvesam@sheridancollege.ca>
2024-12-11 12:53:34 -0500

    the real test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized ehhhh Node.js App!');
+    res.send('Hello Samira here');
 });
 
 app.listen(PORT, () => {
